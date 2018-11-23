a2d6b9501e814384040bcbeb76cf68c7910f2acd
Stan <stvanbaarsen@gmail.com>
2018-11-23 22:34:34 +0100

    add search plugins (reddit, youtube)

--- Pages/Main/JS/Search.js
+++ Pages/Main/JS/Search.js
@@ -1,14 +1,46 @@
+patterns = [
+	{
+		shortcuts: [
+			'yt', 'ytsearch', 'youtube', 'youtubesearch'
+		],
+		icon: undefined,
+		url: 'https://www.youtube.com/results?search_query='
+	},
+	{
+		shortcuts: [
+			'rd', 'reddit', 'rdsearch', 'redditsearch'
+		],
+		icon: undefined,
+		url: 'https://www.reddit.com/search?q='
+	},
+]
+
 function search() {
-	if ($("#search").val().length) {
-		if (isURL($("#search").val())) {
-			sv = $("#search").val();
-			if (sv.startsWith("http")) {
-				location = sv;
-			} else {
-				location = 'https://' + sv;
+	sV = $("#search").val();
+	defaultB = 'https://www.google.com/search?q=';
+	BASE = defaultB;
+	sQ = sV;
+
+	if (sV.length) {
+		for (let p = 0; p < patterns.length; p++) {
+			for (let q = 0; q < patterns[p].shortcuts.length; q++) {
+				const sc = patterns[p].shortcuts[q];
+				if(sV.startsWith(sc + ":")){
+					BASE = patterns[p].url;
+					sQ = sV.split(sc + ":")[1];
+					break;
+				}
 			}
-		} else {
-			location = 'https://www.google.com/search?q=' + encodeURIComponent($("#search").val());
+		}
+
+		if(BASE != defaultB || !isURL(sV)) {
+			location = BASE + encodeURIComponent(sQ);			
+		}else{
+			if (sv.startsWith("http")) {
+				location = sV;
+			} else {
+				location = 'https://' + sV;
+			}
 		}
 	} else {
 		redirectToProjects();
