d078f813e7ba9fceb6c9a6d758d52af5db71bb96
Stan <stvanbaarsen@gmail.com>
2018-12-02 16:48:19 +0100

    start working on weather page

--- Pages/Weather/JS/Weather.js
+++ Pages/Weather/JS/Weather.js
@@ -1,3 +1,7 @@
+var wURL = 'https://api.openweathermap.org/data/2.5/weather?APPID=e98a229cdc17ffdc226168c33aefa0c1&q=';
+
+var wLOC;
+
 $(document).ready(function(){
 	$("#back").on("click", function () {
 		$("#main").css("left", '100%');
@@ -11,4 +15,20 @@ $(document).ready(function(){
 	setTimeout(function(){
 		$("#main").css("opacity", 1);
 	}, 100);
+
+
+	get("wLOC", function(a){
+		wLOC = a.wLOC;
+		$(".title").html($(".title").attr("data-def") + wLOC);
+
+		loadWeather();
+	});
 });
+
+
+function loadWeather(){
+	$.getJSON(wURL + wLOC, function(data){
+		console.log(data);
+		$("#weather").html(Math.round((data.main.temp - 273.15)*10)/10 + "&deg;C<br>Dit is zegmaar nog niet af.");
+	});
+}
