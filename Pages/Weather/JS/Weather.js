f2419669ae582a0d66515c9e30de8fc8dd51f755
Stan <stvanbaarsen@gmail.com>
2018-12-03 19:51:22 +0100

    Improve weather page

--- Pages/Weather/JS/Weather.js
+++ Pages/Weather/JS/Weather.js
@@ -2,7 +2,7 @@ var wURL = 'https://api.openweathermap.org/data/2.5/weather?APPID=e98a229cdc17ff
 
 var wLOC;
 
-$(document).ready(function(){
+$(document).ready(function () {
 	$("#back").on("click", function () {
 		$("#main").css("left", '100%');
 		setTimeout(function () {
@@ -12,12 +12,12 @@ $(document).ready(function(){
 
 	$("#main").addClass("loaded");
 
-	setTimeout(function(){
+	setTimeout(function () {
 		$("#main").css("opacity", 1);
 	}, 100);
 
 
-	get("wLOC", function(a){
+	get("wLOC", function (a) {
 		wLOC = a.wLOC;
 		$(".title").html($(".title").attr("data-def") + wLOC);
 
@@ -26,13 +26,55 @@ $(document).ready(function(){
 });
 
 
-function loadWeather(){
-	$.getJSON(wURL + wLOC, function(data){
-		console.log(data);
-		$("#temp").html(Math.round((data.main.temp - 273.15)*10)/10 + "&deg;C");
-		data.wind.deg
-		wdrs = ['N', 'NO', 'O', 'ZO', 'Z', 'ZW', 'W', 'NW'];
+function loadWeather() {
+	$.getJSON(wURL + wLOC, function (data) {
+		$("#temp").html(Math.round((data.main.temp - 273.15) * 10) / 10 + "&deg;C");
+		wd = data.wind.deg;
+		wdrs = ['Z', 'ZW', 'W', 'NW', 'N', 'NO', 'O', 'ZO', 'Z'];
+		var wdn;
+		if (wd < 22.5 || wd >= 360 - 22.5) {
+			wdn = 'N'
+		}
+		if (wd >= 22.5 && wd < 45 + 22.5) {
+			wdn = 'NO'
+		}
+		if (wd >= 45 + 22.5 && wd < 90 + 22.5) {
+			wdn = 'O'
+		}
+		if (wd >= 90 + 22.5 && wd < 135 + 22.5) {
+			wdn = 'ZO'
+		}
+		if (wd >= 135 + 22.5 && wd < 180 + 22.5) {
+			wdn = 'Z'
+		}
+		if (wd >= 180 + 22.5 && wd < 225 + 22.5) {
+			wdn = 'ZW'
+		}
+		if (wd >= 225 + 22.5 && wd < 270 + 22.5) {
+			wdn = 'W'
+		}
+		if (wd >= 270 + 22.5 && wd < 315 + 22.5) {
+			wdn = 'NW'
+		}
+		if(!wdn) wdn = '&mdash;';
 
-		$("#wdesc").html()
+
+		$("#wdesc").html('<b>Windrichting</b>: ' + wdn);
+		$("#wsp").html('<b>Windsnelheid</b>: ' + data.wind.speed + "m/s");
+
+		$("#wA").css("transform", 'rotate(' + (wd - 360) + "deg)");
+
+		$("#cloudiness").html(data.clouds.all + "% bewolkt");
+
+		$("#sr").html('<b>Zonsopkomst</b>: ' + new Date(data.sys.sunrise*1000).getHours() + ":" + new Date(data.sys.sunrise*1000).getMinutes());
+		$("#ss").html('<b>Zonsondergang</b>: ' + new Date(data.sys.sunset*1000).getHours() + ":" + new Date(data.sys.sunrise*1000).getMinutes());
+
+		$("#wc").html(data.weather[0].description.toSentenceCase());
+
+		$("#hum").html('<b>Luchtvochtigheid</b>: ' + data.main.humidity);
 	});
+}
+
+String.prototype.toSentenceCase = function(){
+	return this[0].toUpperCase() + (this[1]?this.substr(1).toLowerCase():'');
 }
